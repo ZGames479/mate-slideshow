62e306b13ff4a17556e6e15c9882dc6f2648fa29
ZGames479 <32884271+ZGames479@users.noreply.github.com>
2022-06-13 10:36:25 -0500

    Add files via upload

---
+++ built/app.d.ts
@@ -0,0 +1,19 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+import * as MRE from '@microsoft/mixed-reality-extension-sdk';
+/**
+ * The main class of this app. All the logic goes here.
+ */
+export default class MyHelloWorld {
+    private context;
+    private kitObject;
+    private kitObjectIds;
+    private assets;
+    constructor(context: MRE.Context);
+    private started;
+    getId(): string;
+    timerFunction(that: any): void;
+}
+//# sourceMappingURL=app.d.ts.map
